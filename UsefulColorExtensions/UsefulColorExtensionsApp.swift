2fe8128f9d0e255c64392b67c90ae8b3f557a492
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-12-06 23:40:58 +0900

    Initial Commit

---
+++ UsefulColorExtensions/UsefulColorExtensionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsefulColorExtensionsApp.swift
+//  UsefulColorExtensions
+//
+//  Created by joe on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct UsefulColorExtensionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
